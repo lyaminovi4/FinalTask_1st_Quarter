cf835d268f0eed15856c3a0b82030a0b846b8afe
Constantine <lyaminovi4@gmail.com>
2023-01-27 18:08:58 +0500

    add Task

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+// При решение не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+string[] array1 = new string[7] {"Hi", "Hello", "Git", "World", "and", "GitHub", "C#"};
+string[] array2 = new string[array1.Length];
+
+void Array(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {   
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+Array(array1, array2);
+PrintArray(array2);
